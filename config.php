98078c41c758fa2971cf085bf52b185e7996d97c
C.L <chiholam@users.noreply.github.com>
2020-03-24 10:21:59 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('HOST_KEY', 'ct8f24047a886371103f5a34861fd5e9');
+define('HOST_MAIL', 'cloudflarepartner@cloudflare.com');
+// $page_title = "CHUNGTAT"; // Optional. Should not use HTML special character.
+// $tlo_path = '/'; // Optional. The installation path for this panel, ending with '/'. Required for HTTP/2 Push.
+// $is_debug = false; // Enable debug mode
